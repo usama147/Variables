2df489b6918459a7db9726e9fad1847225e5ed4e
usama147 <gierdienusama@gmail.com>
2020-03-09 11:26:18 +0200

    Learning python language.

---
+++ Variables.py
@@ -0,0 +1,14 @@
+# This is an illustration of using the print keyword and display text.
+print ("Good Morning")
+print ("Over the weekend I spent some quality time with my older brother.")
+print ("We played and finished all 5 Metal slug games.") # It was quite easy.
+
+x= 40
+y= 55.6
+z= "Gold"
+AB= 3
+print (x,y,z,AB) # Prints all three variables.
+str_multiline="""Iam learning python basics.
+Its cool to learn a new language.
+Hey the weekend was fun."""
+print (str_multiline) # Prints variable in three separate lines.
